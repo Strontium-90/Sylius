0899ad2e6d77700040d039771f9df15da3b31eb2
Aleksey Bannov <Aleksey.Bannov@noveogroup.com>
2015-06-22 12:52:32 +0300

    default repository

--- src/Sylius/Bundle/InstallerBundle/Command/InstallAssetsCommand.php
+++ src/Sylius/Bundle/InstallerBundle/Command/InstallAssetsCommand.php
@@ -2,7 +2,6 @@
 
 namespace Sylius\Bundle\InstallerBundle\Command;
 
-use RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -19,8 +18,7 @@ class InstallAssetsCommand extends AbstractInstallCommand
             ->setHelp(<<<EOT
 The <info>%command.name%</info> command downloads and installs all Sylius media assets.
 EOT
-            )
-        ;
+            );
     }
 
     /**
@@ -29,6 +27,11 @@ EOT
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln(sprintf('Installing Sylius assets for environment <info>%s</info>.', $this->getEnvironment()));
+
+        $rep = $this->get('sylius.repository.api_user');
+        $manager = $this->get('sylius.manager.api_user');
+        $rep2 = $manager->getRepository($this->getContainer()->getParameter('sylius.model.api_user.class'));
+
 
         $commands = array(
             'assets:install',
